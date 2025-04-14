9e8be035f79feb2f88d915442ad290bd2843c1aa
Jakob Wessel <jakob.wessel@posteo.de>
2025-04-14 16:54:16 +0100

    Case studies on new Potsdam station data + new plotting function of mean of monthly maxima instead of maximum of monthly maxima

--- weathergen/plotting.py
+++ weathergen/plotting.py
@@ -134,6 +134,34 @@ def get_max_plot(obs, pred, time, ax=None, levels=[0.9, 0.8, 0.7, 0.5]):
     ax.set_xlabel("Month")
 
 
+def get_mean_of_max_plot(obs, pred, time, ax=None, levels=[0.9, 0.8, 0.7, 0.5]):
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    df_preds = pd.DataFrame(pred.squeeze().T)
+    df_preds["time"] = time
+    df_preds = df_preds.melt(id_vars="time", value_name="pred")
+    df_preds["year"] = df_preds.time.dt.year
+    df_preds["month"] = df_preds.time.dt.month
+    df_preds = df_preds.groupby([df_preds.year, df_preds.month, df_preds.variable])["pred"].max().reset_index()
+    df_preds = df_preds.groupby([df_preds.month, df_preds.variable]).mean()
+
+    df_obs = pd.DataFrame({"obs": obs.squeeze(), "time": time})
+    df_obs["year"] = df_obs.time.dt.year
+    df_obs["month"] = df_obs.time.dt.month
+    df_obs = df_obs.groupby([df_obs.year, df_obs.month])["obs"].max().reset_index()
+    df_obs = df_obs.groupby(df_obs.month).mean()
+
+    for level in levels:
+        sns.lineplot(
+            df_preds, x="month", y="pred", ax=ax, linestyle="", errorbar=partial(ci_sims, level=level), legend=False
+        )
+    sns.lineplot(df_obs, x="month", y="obs", ax=ax, color="black")
+    ax.set_title("Max")
+    ax.set_ylabel("")
+    ax.set_xlabel("Month")
+
+
 def get_min_plot(obs, pred, time, ax=None, levels=[0.9, 0.8, 0.7, 0.5]):
     if ax is None:
         fig, ax = plt.subplots()
@@ -267,39 +295,42 @@ def get_prop_wet_plot(obs, pred, time, ax=None, levels=[0.9, 0.8, 0.7, 0.5]):
     ax.set_xlabel("Month")
 
 
-def get_temp_precip_cor_plot(obs_temp, pred_temp, obs_precip, pred_precip, time, ax=None, levels=[0.9, 0.8, 0.7, 0.5]):
+def get_cor_plot(
+    obs_1, pred_1, obs_2, pred_2, time, var_1_name="", var_2_name="", ax=None, levels=[0.9, 0.8, 0.7, 0.5]
+):
     if ax is None:
         fig, ax = plt.subplots()
 
-    df_preds = pd.DataFrame(pred_temp.squeeze().T)
+    df_preds = pd.DataFrame(pred_1.squeeze().T)
     df_preds["time"] = time
-    df_preds = df_preds.melt(id_vars="time", value_name="temp")
+    df_preds = df_preds.melt(id_vars="time", value_name="v1")
 
-    df_preds_precip = pd.DataFrame(pred_precip.squeeze().T)
-    df_preds_precip["time"] = time
-    df_preds_precip = df_preds_precip.melt(id_vars="time", value_name="precip")
+    df_preds_var_2 = pd.DataFrame(pred_2.squeeze().T)
+    df_preds_var_2["time"] = time
+    df_preds_var_2 = df_preds_var_2.melt(id_vars="time", value_name="v2")
 
-    df_preds = df_preds.merge(df_preds_precip, on=["time", "variable"])
+    df_preds = df_preds.merge(df_preds_var_2, on=["time", "variable"])
 
     df_preds = (
-        df_preds.groupby([df_preds.time.dt.month, df_preds.variable])[["temp", "precip"]]
-        .corr()
-        .iloc[0::2, -1]
-        .reset_index()
+        df_preds.groupby([df_preds.time.dt.month, df_preds.variable])[["v1", "v2"]].corr().iloc[0::2, -1].reset_index()
     )
 
-    df_obs = pd.DataFrame({"temp": obs_temp.squeeze(), "precip": obs_precip.squeeze(), "time": time})
-    df_obs = df_obs.groupby(df_obs.time.dt.month)[["temp", "precip"]].corr().iloc[0::2, -1].reset_index()
+    df_obs = pd.DataFrame({"v1": obs_1.squeeze(), "v2": obs_2.squeeze(), "time": time})
+    df_obs = df_obs.groupby(df_obs.time.dt.month)[["v1", "v2"]].corr().iloc[0::2, -1].reset_index()
 
     for level in levels:
         sns.lineplot(
-            df_preds, x="time", y="precip", ax=ax, linestyle="", errorbar=partial(ci_sims, level=level), legend=False
+            df_preds, x="time", y="v2", ax=ax, linestyle="", errorbar=partial(ci_sims, level=level), legend=False
         )
-    sns.lineplot(df_obs, x="time", y="precip", ax=ax, color="black")
-    sns.lineplot(df_obs, x="time", y="precip", ax=ax, color="black")
-    ax.set_title("Corr temp - precip")
+    sns.lineplot(df_obs, x="time", y="v2", ax=ax, color="black")
+    sns.lineplot(df_obs, x="time", y="v2", ax=ax, color="black")
+    ax.set_title(f"Corr {var_1_name} - {var_2_name}")
     ax.set_ylabel("")
     ax.set_xlabel("Month")
+
+
+def get_temp_precip_cor_plot(obs_temp, pred_temp, obs_precip, pred_precip, time, ax=None, levels=[0.9, 0.8, 0.7, 0.5]):
+    get_cor_plot(obs_temp, pred_temp, obs_precip, pred_precip, time, "temp", "precip", ax=ax, levels=levels)
 
 
 # Grids of overview plots
