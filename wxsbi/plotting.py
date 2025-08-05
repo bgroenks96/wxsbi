92591ac5e20948a80b7aa20f975e61a117224695
Jakob Wessel <jakob.wessel@posteo.de>
2025-08-05 10:08:39 +0100

    Update to plotting function for better labels

--- wxsbi/plotting.py
+++ wxsbi/plotting.py
@@ -2,31 +2,61 @@ import matplotlib.pyplot as plt
 
 from .wxsbi import SBIResults
 
+
 def plot_target_densitites_sbi(results: SBIResults, cmap="Dark2"):
     summary_target = results.summary_target
     summary_names = results.simulator.summarizer.names
-    fig, axs = plt.subplots(1, summary_target.shape[0], figsize = (summary_target.shape[0]*5, 5))
+    fig, axs = plt.subplots(1, summary_target.shape[0], figsize=(summary_target.shape[0] * 5, 5))
     cmap = plt.cm.get_cmap(cmap).colors
     for i in range(summary_target.shape[0]):
         handles = []
         if "calibration_posterior" in results.simulations:
-            _, _, h1 = axs[i].hist(results.simulations["calibration_posterior"][:, i], color=cmap[0], bins=20, density=True, alpha=0.5)
-            handles.append(h1)
+            _, _, h1 = axs[i].hist(
+                results.simulations["calibration_posterior"][:, i],
+                color=cmap[0],
+                bins=20,
+                density=True,
+                alpha=0.5,
+                label="Calibration posterior",
+            )
         if "calibration_posterior_mean" in results.simulations:
-            _, _, h2 = axs[i].hist(results.simulations["calibration_posterior_mean"][:, i], color=cmap[1], bins=20, density=True, alpha=0.5)
-            handles.append(h2)
+            _, _, h2 = axs[i].hist(
+                results.simulations["calibration_posterior_mean"][:, i],
+                color=cmap[1],
+                bins=20,
+                density=True,
+                alpha=0.5,
+                label="Calibration posterior mean",
+            )
         if "sbi_prior" in results.simulations:
-            _, _, h3 = axs[i].hist(results.simulations["sbi_prior"][:, i], color="gray", bins=30, density=True, alpha=0.5)
-            handles.append(h3)
+            _, _, h3 = axs[i].hist(
+                results.simulations["sbi_prior"][:, i],
+                color="gray",
+                bins=20,
+                density=True,
+                alpha=0.5,
+                label="SBI prior",
+            )
         if "sbi_posterior" in results.simulations:
-            _, _, h4 = axs[i].hist(results.simulations["sbi_posterior"][:, i], color=cmap[2], bins=30, density=True, alpha=0.5)
-            handles.append(h4)
+            _, _, h4 = axs[i].hist(
+                results.simulations["sbi_posterior"][:, i],
+                color=cmap[2],
+                bins=20,
+                density=True,
+                alpha=0.5,
+                label="SBI posterior",
+            )
         if "sbi_posterior_map" in results.simulations:
-            _, _, h5 = axs[i].hist(results.simulations["sbi_posterior_map"][:, i], color=cmap[3], bins=30, density=True, alpha=0.5)
-            handles.append(h5)
-        axs[i].set_title(summary_names[i])
-        target_line = axs[i].axvline(summary_target[i].flatten(), color= "black", label="Target")
-        handles.append(target_line)
-    labels = ["Calibration", "Calibration (mean)", "SBI Prior", "SBI posterior", "SBI posterior (MAP)"]
+            _, _, h5 = axs[i].hist(
+                results.simulations["sbi_posterior_map"][:, i],
+                color=cmap[3],
+                bins=20,
+                density=True,
+                alpha=0.5,
+                label="SBI posterior MAP",
+            )
+        # axs[i].set_title(summary_names[i])
+        target_line = axs[i].axvline(summary_target[i].flatten(), color="black", label="Target")
+    handles, labels = axs[0].get_legend_handles_labels()
     fig.legend(handles=handles, labels=labels, loc="lower center", bbox_to_anchor=(0.5, -0.1), ncol=6)
     return fig, axs
