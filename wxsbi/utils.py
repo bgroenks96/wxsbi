65d0358e082349d13d3c8a46c6a962373a093538
Jakob Wessel <jakob.wessel@posteo.de>
2025-11-10 10:24:06 +0000

    Added manual j2t and t2j functions that work with later versions of jax
    https://github.com/lucidrains/jax2torch/issues/6

--- wxsbi/utils.py
+++ wxsbi/utils.py
@@ -1,16 +1,74 @@
+from functools import partial, wraps
+from inspect import signature
+
 import jax
 import jax.numpy as jnp
-
 import numpyro
 import numpyro.distributions as dist
-
 import torch
-import torch.distributions as torchdist
+from jax import dlpack as jax_dlpack
+from jax.tree_util import tree_map
+from packaging import version
+from torch.utils import dlpack as torch_dlpack
 
-from collections import OrderedDict
-from functools import partial
 
-from jax2torch.jax2torch import jax2torch, j2t, t2j
+def j2t(x_jax):
+    # https://github.com/lucidrains/jax2torch/issues/6
+
+    if version.parse(jax.__version__) < version.parse("0.4.13"):
+        x_torch = torch_dlpack.from_dlpack(jax_dlpack.to_dlpack(x_jax))
+    else:
+        x_torch = torch_dlpack.from_dlpack(x_jax)
+    return x_torch
+
+
+def t2j(x_torch):
+    # https://github.com/lucidrains/jax2torch/issues/6
+    x_torch = x_torch.contiguous()  # https://github.com/google/jax/issues/8082
+
+    if version.parse(jax.__version__) < version.parse("0.4.13"):
+        x_jax = jax_dlpack.from_dlpack(torch_dlpack.to_dlpack(x_torch))
+    else:
+        x_jax = jax_dlpack.from_dlpack(x_torch)
+    return x_jax
+
+
+def tree_t2j(x_torch):
+    return tree_map(lambda t: t2j(t) if isinstance(t, torch.Tensor) else t, x_torch)
+
+
+def tree_j2t(x_jax):
+    return tree_map(lambda t: j2t(t) if isinstance(t, jnp.ndarray) else t, x_jax)
+
+
+def jax2torch(fn):
+    @wraps(fn)
+    def inner(*args, **kwargs):
+        class JaxFun(torch.autograd.Function):
+            @staticmethod
+            def forward(ctx, *args):
+                args = tree_t2j(args)
+                y_, ctx.fun_vjp = jax.vjp(fn, *args)
+                return tree_j2t(y_)
+
+            @staticmethod
+            def backward(ctx, *grad_args):
+                grad_args = (
+                    tree_t2j(grad_args) if len(grad_args) > 1 else t2j(grad_args[0])
+                )
+                grads = ctx.fun_vjp(grad_args)
+                grads = tuple(
+                    map(lambda t: t if isinstance(t, jnp.ndarray) else None, grads)
+                )
+                return tree_j2t(grads)
+
+        sig = signature(fn)
+        bound = sig.bind(*args, **kwargs)
+        bound.apply_defaults()
+        return JaxFun.apply(*bound.arguments.values())
+
+    return inner
+
 
 class NumPyro2TorchDistribution:
     def __init__(self, numpyro_dist, rng_key=jax.random.PRNGKey(0)):
@@ -31,18 +89,19 @@ class NumPyro2TorchDistribution:
     @partial(jax.jit, static_argnums=[0])
     def _log_prob(self, x):
         return self.dist.log_prob(x)
-    
+
+
 class Torch2NumPyroDistribution(dist.Distribution):
     def __init__(self, torch_dist):
         self.dist = torch_dist
         super().__init__(self)
-    
+
     def sample(self, rng_key, shape):
         # set torch global rng seed;
         # ideally there should be a way to set it only locally,
         # but unfortunately pytorch only provides a global option
         torch.manual_seed(rng_key[-1])
         return t2j(self.dist.sample(shape))
-    
+
     def log_prob(self, x):
         return t2j(self.dist.log_prob(j2t(x)))
